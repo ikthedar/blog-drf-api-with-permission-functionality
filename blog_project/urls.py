0c657dc05d291622c907dd6f4213f21a5dcb9082
Ikthedar Chowdhury <ikchy1994@gmail.com>
2024-12-01 23:30:32 +0600

    Uploading initial blog_project and post apps

---
+++ blog_project/urls.py
@@ -0,0 +1,30 @@
+"""
+URL configuration for blog_project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('posts.urls')),
+    path('api-auth/', include('rest_framework.urls')), # new
+]
+
+# Note that since our only app at this point is posts we can include it directly here. If
+# we had multiple apps in a project it might make more sense to create a dedicated api
+# app and then include all the other API url routes into it. But for basic projects like this
+# one, I prefer to avoid an api app that is just used for routing. We can always add one
+# later, if needed.
