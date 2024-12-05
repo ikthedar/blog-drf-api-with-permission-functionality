7ca57a5749eefd7a75a26d0239f427b3c9ea8352
Ikthedar Chowdhury <ik_chy@Ikthedar.>
2024-12-05 12:09:24 +0600

    added two endpoints for Users

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,8 @@
 from rest_framework import generics, permissions
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 from .permissions import isAuthorOrReadOnly
-
+from django.contrib.auth import get_user_model
 
 class PostList(generics.ListCreateAPIView):
 #    permission_classes = (permissions.IsAuthenticated,) # removed this line since we added project-level permission
@@ -16,6 +16,14 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = PostSerializer
 
 
+class UserList(generics.ListCreateAPIView): # new
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    #permission_classes = (isAuthorOrReadOnly,)
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
 
 # At the top of the file we import generics from Django REST Framework as well as
 # our models and serializers files. Then we create two views. PostList uses the generic
